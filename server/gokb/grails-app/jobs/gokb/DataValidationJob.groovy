b1a56cbb6ae902a8ea8325f23a80af485b4953f2
latusaki@gmail.com <latusaki@gmail.com>
2015-03-13 09:55:37 +0000

    Rename job, avoid duplicate tasks

--- server/gokb/grails-app/jobs/gokb/DataValidationJob.groovy
+++ server/gokb/grails-app/jobs/gokb/DataValidationJob.groovy
@@ -2,33 +2,32 @@ package gokb
 
 import org.gokb.cred.*
 
-class TitleCoverageConflictsJob {
+class DataValidationJob {
   static final String CAUSE = "TIPP Coverage extends beyond title publication date"
   static final String ACTION = "Review TIPP coverage date"  
-  // Every five minutes
+  // Every ten minutes
   static triggers = {
-    cron name: 'TitleCoverageConflictsJobTrigger', cronExpression: "0/10 * * * * ?", startDelay:60000
+    cron name: 'DataValidationJobTrigger', cronExpression: "* 0/10 * * * ?", startDelay:60000
   }
 
   def execute() {
-    log.debug("Executing TitleCoverageConflictsJobTrigger")
-    scanTipps();
+    tippCoverageConflicts();
   }
 
-  def scanTipps() {
+  def tippCoverageConflicts() {
     def status_current = RefdataCategory.lookupOrCreate(KBComponent.RD_STATUS, KBComponent.STATUS_CURRENT)
 
     def hql_query = "select tipp from TitleInstancePackagePlatform tipp, Combo as c where c.toComponent=tipp and c.type.value='TitleInstance.Tipps' and (c.fromComponent.publishedFrom < tipp.accessStartDate OR c.fromComponent.publishedTo > tipp.accessEndDate))"
 
     def violating_tipps = TitleInstancePackagePlatform.executeQuery(hql_query)
-
+    def review_deleted =  RefdataCategory.lookupOrCreate('ReviewRequest.Status', 'Deleted')
     violating_tipps.each{ tipp ->
 
-      if(tipp.reviewRequests?.find{it.descriptionOfCause == CAUSE}){
+      if(tipp.reviewRequests?.find{it.descriptionOfCause == CAUSE && it.status != review_deleted }){
         //review already raised
       }else{
         ReviewRequest.raise(tipp,ACTION,CAUSE)
-        log.debug("Generated review request for ${tipp}")
+        // log.debug("Generated review request for ${tipp}")
       }
     }
   }
