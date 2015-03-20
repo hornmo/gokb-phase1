5693b70805a6712e69bd740f91b1646b6b8d55f8
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-03-20 12:11:27 +0000

    Added book and journal layouts, added config

--- server/gokb/grails-app/conf/Config.groovy
+++ server/gokb/grails-app/conf/Config.groovy
@@ -1022,6 +1022,8 @@ globalDisplayTemplates = [
   'org.gokb.cred.Org': [ type:'staticgsp', rendername:'org' ],
   'org.gokb.cred.Platform': [ type:'staticgsp', rendername:'platform' ],
   'org.gokb.cred.TitleInstance': [ type:'staticgsp', rendername:'title' ],
+  'org.gokb.cred.BookInstance': [ type:'staticgsp', rendername:'book' ],
+  'org.gokb.cred.JournalInstance': [ type:'staticgsp', rendername:'journal' ],
   'org.gokb.cred.TitleInstancePackagePlatform': [ type:'staticgsp', rendername:'tipp' ],
   'org.gokb.refine.Rule': [ type:'staticgsp', rendername:'rule' ],
   'org.gokb.refine.RefineProject': [ type:'staticgsp', rendername:'project' ],
