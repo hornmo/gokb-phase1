c28b9700f209158d32129bcc5f6d4676c88d4628
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-04-16 16:31:50 +0100

    De componentise{eep} person - lost my bottle

--- server/gokb/grails-app/domain/org/gokb/cred/Person.groovy
+++ server/gokb/grails-app/domain/org/gokb/cred/Person.groovy
@@ -2,12 +2,16 @@ package org.gokb.cred
 
 import javax.persistence.Transient
 
-class Person extends KBComponent {
+class Person {
+
+  String label;
 
   static mapping = {
+    label column:'subj_heading'
   }
 
   static constraints = {
+    label(nullable:false, blank:false)
   }
 
 }
