d3c4bac56792f0c8bf610c73b6252442e98b8086
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-07-20 08:00:34 +0100

    Add code to enable auto create of test user accounts from bootstrap and sysconfig

--- server/gokb/grails-app/conf/BootStrap.groovy
+++ server/gokb/grails-app/conf/BootStrap.groovy
@@ -151,6 +151,43 @@ class BootStrap {
       }
     }
 
+    grailsApplication.config.sysusers.each { su ->
+      log.debug("test ${su.name} ${su.pass} ${su.display} ${su.roles}");
+      def user = User.findByUsername(su.name)
+      if ( user ) {
+        if ( user.password != su.pass ) {
+          log.debug("Hard change of user password from config ${user.password} -> ${su.pass}");
+          user.password = su.pass;
+          user.save(failOnError: true)
+        }
+        else {
+          log.debug("${su.name} present and correct");
+        }
+      }
+      else {
+        log.debug("Create user...");
+        user = new User(
+                      username: su.name,
+                      password: su.pass,
+                      display: su.display,
+                      email: su.email,
+                      enabled: true).save(failOnError: true)
+      }
+
+      log.debug("Add roles for ${su.name}");
+      su.roles.each { r ->
+        def role = Role.findByAuthority(r)
+        if ( ! ( user.authorities.contains(role) ) ) {
+          log.debug("  -> adding role ${role}");
+          UserRole.create user, role
+        }
+        else {
+          log.debug("  -> ${role} already present");
+        }
+      }
+    }
+
+
     String fs = grailsApplication.config.project_dir
     log.debug("Theme:: ${grailsApplication.config.gokb.theme}");
 
