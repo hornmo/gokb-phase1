f7c35f85c7e94142f58a81a6121f11a95e228488
Ian Ibbotson <ianibbo@gmail.com>
2015-11-14 20:11:45 +0000

    Added data object

---
+++ server/gokb/test/functional/Data.groovy
@@ -0,0 +1,8 @@
+class Data {
+    static sep = File.separator
+    static workingDir = "${System.getProperty('user.dir')}" + sep + ".." + sep
+
+    static admin_username = "admin"
+    static admin_password = "admin"
+
+}
