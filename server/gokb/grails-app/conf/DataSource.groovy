b563b4b9e5566b033645c9b9e1ec51f50104c1bf
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2015-11-23 15:33:08 +0000

    Changed the source to pg sql.

--- server/gokb/grails-app/conf/DataSource.groovy
+++ server/gokb/grails-app/conf/DataSource.groovy
@@ -13,27 +13,49 @@ hibernate {
 environments {
 
     development {
+//        dataSource {
+//            dbCreate = "update"
+//            driverClassName = "com.mysql.jdbc.Driver"
+//            dialect=org.hibernate.dialect.MySQL5Dialect
+//            username = "k-int"
+//            password = "k-int"
+//            url = "jdbc:mysql://localhost/GoKBLabsDev?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
+//            pooled = true
+//            configClass = 'com.k_int.KIGormConfiguration'
+//            properties {
+//                maxActive = 500
+//                minEvictableIdleTimeMillis=1800000
+//                timeBetweenEvictionRunsMillis=1800000
+//                numTestsPerEvictionRun=3
+//                testOnBorrow=true
+//                testWhileIdle=true
+//                testOnReturn=true
+//                validationQuery="select 1"
+//                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_SERIALIZABLE
+//            }
+//        }
         dataSource {
-            dbCreate = "update"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect=org.hibernate.dialect.MySQL5Dialect
-            username = "k-int"
-            password = "k-int"
-            url = "jdbc:mysql://localhost/GoKBLabsDev?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
-            pooled = true
-            configClass = 'com.k_int.KIGormConfiguration'
-            properties {
-                maxActive = 500
-                minEvictableIdleTimeMillis=1800000
-                timeBetweenEvictionRunsMillis=1800000
-                numTestsPerEvictionRun=3
-                testOnBorrow=true
-                testWhileIdle=true
-                testOnReturn=true
-                validationQuery="select 1"
-                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_SERIALIZABLE
-            }
-        }
+          dbCreate = "update"
+          driverClassName = "org.postgresql.Driver"
+          dialect = org.hibernate.dialect.PostgreSQLDialect
+          username = "k-int"
+          password = "k-int"
+          url = "jdbc:postgresql://localhost:5432/GOKb"
+          pooled = true
+          formatSql=true
+          logSql=true
+          properties {
+              maxActive = 500
+              minEvictableIdleTimeMillis=1800000
+              timeBetweenEvictionRunsMillis=1800000
+              numTestsPerEvictionRun=3
+              testOnBorrow=true
+              testWhileIdle=true
+              testOnReturn=true
+              validationQuery="select 1"
+              defaultTransactionIsolation = java.sql.Connection.TRANSACTION_SERIALIZABLE
+          }
+      }
     }
     test {
       dataSource {
